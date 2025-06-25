49aa9a596153017ed043ddb47d896e73b25a6ad6
DannyJoy01 <dannyjoy678@gmail.com>
2025-06-25 21:47:15 +0100

    fix: corrected import path case for Teaching component

--- src/pages/About-Page/aboutPage.jsx
+++ src/pages/About-Page/aboutPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Nav from '../../components/nav/nav.jsx';
 import AboutIntro from '../../components/About-Intro/aboutIntro.jsx';
-import Teaching from '../../components/Teaching/teaching.jsx';
-import Footer from '../../components/Footer/footer.jsx';
+import Teaching from '../../components/teaching/teaching.jsx';
+import Footer from '../../components/footer/Footer.jsx';
 const AboutPage = () => {
   return (
     <div>
