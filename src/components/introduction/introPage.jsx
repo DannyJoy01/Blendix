32d67a4b60386974aa3931b37b8f777f571a0c86
DannyJoy01 <dannyjoy678@gmail.com>
2025-07-02 14:50:39 +0100

    latest commit

--- src/components/introduction/introPage.jsx
+++ src/components/introduction/introPage.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const IntroPage = () => {
   return (
     <div className="w-full h-[280px] pt-[330px] pb-[200px] md:h-[350px] flex justify-center items-center md:pt-[250px] font-pop m:">.
-      <div className="w-[86%] p-14 rounded-3xl md:w-[77%] m:w-[100%] m:h-[35  0px] m:p-12 bg-dix_blue h-[450px] md:h-[380px] p-24 md:p-18 flex flex-col justify-center gap-5 items-start text-start rounded-2xl">
+      <div className="w-[86%] p-14 rounded-3xl md:w-[77%] m:w-[100%] m:h-[390px] m:p-12 bg-dix_blue h-[450px] md:h-[380px] p-24 md:p-18 flex flex-col justify-center gap-5 items-start text-start rounded-2xl">
         <h1 className="font-bold text-[30px] font-inter text-dix_yellow">
           Intro...
         </h1>
-        <p className="font-mtn text-[18px] font-normal md:text-[19px] leading-[27px] md:leading-[27px]">
+        <p className="font-mtn text-[18px] font-normal md:text-[19px] m:text-[16px] leading-[27px] md:leading-[27px] m:leading-[20px]">
           I am a creative thinker and problem solver who thrives in
           collaborative environments. My design journey began with a strong
           foundation in user-centered design principles, which I've consistently
